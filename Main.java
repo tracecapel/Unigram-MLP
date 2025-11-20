b52ed60e09619ef0fe9f479858bda596821cc8ee
tracecapel <116193459+tracecapel@users.noreply.github.com>
2025-11-20 15:51:37 -0500

    Refactor Main class for user input and training updates
    Updated file name from 'train.csv' to 'simple.csv' and modified MLP training iterations. Added user input handling for continuous word prediction.

--- Main.java
+++ Main.java
@@ -2,33 +2,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("train.csv");
-        HashMap<Integer, String> convoMap = Parse.initializeConversationMap(file);
-        HashMap<String, Integer> wordmap = Parse.initializeWordMap(convoMap);
-        Map<Integer, String> reverseMap = new HashMap<>();
-        int[][] output = Tokenize.tokenize(wordmap, convoMap);
-        for (Map.Entry<String, Integer> entry : wordmap.entrySet()) {
-    reverseMap.put(entry.getValue(), entry.getKey());
-}
-System.out.println(reverseMap.get(0));
-        
-        MLP mlp = new MLP(wordmap.size() * 2);
-
-        mlp.train(1, wordmap, convoMap);
-
-        int[] hello = new int[3];
-        hello[0] = (int) wordmap.get("my");
-        hello[1] = (int) wordmap.get("name");
-        hello[2] = (int) wordmap.get("is");
-        
-        int targetWord = mlp.feedNetwork(hello);
-
-    
-    System.out.println(reverseMap.get(targetWord));
+  public static void main(String[] args) throws FileNotFoundException {
+    File file = new File("simple.csv");
+    HashMap<Integer, String> convoMap = Parse.initializeConversationMap(file);
+    HashMap<String, Integer> wordmap = Parse.initializeWordMap(convoMap);
+    Map<Integer, String> reverseMap = new HashMap<>();
+    int[][] output = Tokenize.tokenize(wordmap, convoMap);
+    for (Map.Entry<String, Integer> entry : wordmap.entrySet()) {
+      reverseMap.put(entry.getValue(), entry.getKey());
     }
 
-    
+    Scanner scanner = new Scanner(System.in);
+
+    MLP mlp = new MLP(wordmap.size());
+
+    mlp.train(100, wordmap, convoMap);
+
+    while (true) {
+      String word1 = scanner.nextLine().toLowerCase().trim();
+      int[] input = new int[1];
+
+      int targetWord = mlp.feedNetwork(input);
+
+      System.out.print(reverseMap.get(targetWord));
+      mlp.printNetworkMappings(wordmap);
+    }
+  }
 }
