3e90734ddf511ddc16db560900a5b01bcc542e77
tracecapel <116193459+tracecapel@users.noreply.github.com>
2025-11-23 20:12:51 -0500

    Refactor Perceptron class methods
    Updated the Perceptron class methods for consistency and clarity.

--- Perceptron.java
+++ Perceptron.java
@@ -1,56 +1,53 @@
-public class Perceptron{
-     
-    double x[]; //Inputs
-    double w[]; //Weights
-    double bias; //Bias 
+public class Perceptron {
+  double x[]; // Inputs
+  double w[]; // Weights
+  double bias; // Bias
 
-    public Perceptron(int numInputs){
-        this.x = new double[numInputs];
-        this.w = new double[numInputs];
-        for(int i = 0; i < w.length; i++){
-            w[i] = Math.random() * .1;
-        }
-        this.bias = 0;
+  public Perceptron(int numInputs) {
+    this.x = new double[numInputs];
+    this.w = new double[numInputs];
+    for (int i = 0; i < w.length; i++) {
+      w[i] = Math.random() * .1;
+    }
+    this.bias = 0;
+  }
+
+  // RELU activation
+  public double activate() {
+    double weightedSum = 0;
+
+    for (int i = 0; i < x.length; i++) {
+      weightedSum += x[i] * w[i];
     }
 
+    weightedSum += bias;
 
-    //RELU activation
-    public double activate(){
-        double weightedSum = 0;
+    return 1 / (1 + Math.exp(-weightedSum));
+  }
 
-        for(int i = 0; i < x.length; i++){
-            weightedSum += x[i] * w[i];
-        }
+  // Sigmoid activation
+  public double activateOutput() {
+    double weightedSum = 0;
 
-        weightedSum += bias;
-
-        return Math.max(0, weightedSum);
+    for (int i = 0; i < x.length; i++) {
+      weightedSum += x[i] * w[i];
     }
 
-     //Sigmoid activation
-    public double activateOutput(){
-        double weightedSum = 0;
+    weightedSum += bias;
 
-        for(int i = 0; i < x.length; i++){
-            weightedSum += x[i] * w[i];
-        }
+    return 1 / (1 + Math.exp(-weightedSum));
+  }
 
-        weightedSum += bias;
+  // Weighted sum
+  public double getWeightedSum() {
+    double weightedSum = 0;
 
-        return 1 / (1 + Math.exp(-weightedSum));
+    for (int i = 0; i < x.length; i++) {
+      weightedSum += x[i] * w[i];
     }
 
-    //Weighted sum
-    public double getWeightedSum(){
-        double weightedSum = 0;
+    weightedSum += bias;
 
-        for(int i = 0; i < x.length; i++){
-            weightedSum += x[i] * w[i];
-        }
-
-        weightedSum += bias;
-
-        return weightedSum;
-    } 
-
+    return weightedSum;
+  }
 }
